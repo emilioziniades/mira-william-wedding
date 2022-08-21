4bd9380941358c5afd00784beccf4cef09c4ffc2
Emilio Ziniades <emilioziniades@protonmail.com>
2022-08-21 19:17:18 +0200

    final navbar!

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,53 +1,24 @@
 import React, { useState, useEffect, FC } from "react";
 
 const Navbar: FC = () => {
-  const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+  const navs = ["home", "details", "rsvp", "registry"];
 
-  const controlNavbar = () => {
-    if (typeof window !== "undefined") {
-      if (window.scrollY > lastScrollY) {
-        // if scroll down hide the navbar
-        setShow(false);
-      } else {
-        // if scroll up show the navbar
-        setShow(true);
-      }
-
-      // remember current page location to use in the next move
-      setLastScrollY(window.scrollY);
-    }
+  const handleClick = (event) => {
+    event.preventDefault();
+    const elem = document.getElementById(event.target.innerHTML);
+    const nav = document.getElementById("nav-bar");
+    window.scrollTo(elem.offsetLeft, elem.offsetTop - (nav.offsetHeight + 10));
   };
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", controlNavbar);
-
-      // cleanup function
-      return () => {
-        window.removeEventListener("scroll", controlNavbar);
-      };
-    }
-  }, [lastScrollY]);
-
-  const navs = ["home", "details", "rsvp", "registry"];
   return (
     <nav
-      className={`active sticky top-0 bg-white w-screen p-4 shadow-lg ${
-        show && "hidden"
-      }`}
+      className="active sticky top-0 bg-white w-screen p-4 shadow-lg"
+      id="nav-bar"
     >
       {navs.map((name) => (
         <a
           href={`#${name}`}
-          onClick={(event) => {
-            event.preventDefault();
-            const elem = document.getElementById(event.target.innerHTML);
-            console.log(elem);
-            window.scrollTo(elem.offsetLeft, elem.offsetTop);
-            setShow(false);
-            setLastScrollY(window.scrollY);
-          }}
+          onClick={handleClick}
           className="m-2 uppercase font-bold font-helv text-mblue hover:text-wblue"
           key={name}
         >
